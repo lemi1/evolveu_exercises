60fa3423afd6f897608bed12543d102caf2d0f2f
lemi <lemi.m@outlook.com>
2018-10-22 11:48:40 -0600

    some js exercises

---
+++ logic/t_logic.py
@@ -0,0 +1,32 @@
+ 
+#4) Function is a block a code that runs when its called. The code that is indented under the function is part of the function.
+
+
+
+#5)call it and run it
+
+#6)updates dont have any consqeuences. make sure code is working well or
+#why it is not working.a test can excute if there is no assert statements.
+
+#7)pytest helps you write better programs, finds the function with test_filename.py
+
+#8)1- create a stub which includes def , function name, parameters and return 0 or none.
+	# 2 create tests to fail.
+	#3 then write the code
+	#4 and then test to pass
+	
+#9)*,/,//,+,-,%,**,-a(negation)
+
+
+#10)
+def floor_division(a,b):
+	return 0
+
+
+def exponenation(a,b):
+	return 0
+
+def modilus (a,b):
+	return 0
+
+
---
+++ logic/test_t_logic.py
@@ -0,0 +1,7 @@
+ import unittest
+ import t_logic
+
+ class Testmath(unittest.TestCase):
+ 	def test_floor_division(self):
+ 		self.assertEqual(3,7)
+
