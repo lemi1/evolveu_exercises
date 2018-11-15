efe4caa269815585515321cf2e074c10cb8d6428
evolveu <lemi.m@outlook.com>
2018-11-15 09:20:34 -0700

    linkedlist excercise

---
+++ my-app/src/sum.js
@@ -0,0 +1,21 @@
+ 
+function sum(a, b) {
+    return a + b;
+}
+
+
+function add(a, b){
+    return a + b;
+}
+
+
+//module.exports = sum;
+
+
+class LinkList{
+    play(value){
+        console.log(value);
+        return value;
+    }
+}
+export default {sum, add};
---
+++ my-app/src/sum.test.js
@@ -0,0 +1,18 @@
+ 
+import lm from './sum';
+//const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(lm.sum(1,2)).toBe(3);
+});
+
+
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(lm.add(1,2)).toBe(3);
+});
+
+
+
+
+console.log(lm);
