ef257c74f450f3838a09b3828b373be56bf5fc85
evolveu <lemi.m@outlook.com>
2018-11-27 14:45:43 -0700

    math exercise update

---
+++ React/linked-list/src/components/Community.js
@@ -0,0 +1,8 @@
+class City{
+    constructor(Name, Latitude, Longitude, Population){
+    this.Name = Name;
+    this.Latitude = Latitude;
+    this.Longitude = Longitude;
+    this.Population = Population;
+    }
+} 
--- math/math.test.js
+++
@@ -1,4 +0,0 @@
-
-test('this test will add 2 numbers', () => {
-    let ll = new Lemi.LinkList('frist', 5);
-    expect(ll.show()).toBe('frist, 5');
