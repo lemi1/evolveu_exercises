1e347a924c61a55a6d9e627672ba915c7c37a313
evolveu <lemi.m@outlook.com>
2019-02-12 09:57:13 -0700

    dom

--- dom/script.js
+++ dom/script.js
@@ -13,12 +13,45 @@ document.getElementById("btnAdd").addEventListener("click", function () {
 //     do {
 //         console.log(document.getElementById('list').appendChild(document.createElement('li')) + counter)
 //     }
-//     while (counter > 3);
-//     //counter = counter + 1;
-//     // let ol = document.getElementById('list');
-//     // let li = document.createElement('li');
-//     // counter = counter + 1;
-
-
+//     while
 //     // console.log(ol.appendChild(li).textContent = 'item ' + counter;);
 // });
+
+let count = 1;
+document.getElementById("addy").addEventListener("click", createCard);
+//let parent = document.getElementById('card').addEventListener('click', function(event){
+
+//     console.log("hey", event.target.id)
+// });
+function createCard() {
+
+    let div = document.createElement('div');
+    let id = "divCard" + count;
+    div.setAttribute('id', id);
+    div.style.border = '2px solid black'
+    card.appendChild(div);
+    document.getElementById(id).innerHTML = "<p>Card " + count + "</p> <br/>\n" +
+        "<button onclick='addBefore(event)'>Add Before</button><button onclick='addAfter(event)'> Add After</button ><br/><br/><button onclick='remove(event)'>Delete</button>"
+
+    count++
+    return div;
+}
+function remove(event) {
+    console.log(event.target.parentNode.id)
+    event.target.parentNode.remove()
+
+}
+
+function addBefore(event) {
+    let ref = event.target.parentNode;
+    console.log(ref)
+    card.insertBefore(createCard(), ref)
+
+}
+function addAfter(event) {
+    let ref = event.target.parentNode;
+    card.insertBefore(createCard(), ref.nextSibling)
+
+}
+
+
