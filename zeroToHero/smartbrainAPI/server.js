8ed45dfb5709898c90d6b6465d9e44ab83c3c613
lemi <lemi@pop-os.localdomain>
2019-04-09 12:36:27 -0600

    update

--- zeroToHero/smartbrainAPI/server.js
+++ zeroToHero/smartbrainAPI/server.js
@@ -1,9 +1,44 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
+app.use(bodyParser.json())
+
+const database = {
+    users:[
+        {
+            id:'123',
+            name:'John',
+            email:'john@gmail.com',
+            password:'cookies',
+            enteries:0,
+            joined: new Date()
+        },
+
+        {
+            id:'456',
+            name:'Sally',
+            email:'Sally@gmail.com',
+            password:'apples',
+            enteries:0,
+            joined: new Date()
+        }
+    ]
+}
 
 app.get('/', (req, res) =>{
     res.send('this is working')
+})
+
+app.post('/signin',(req,res) =>{
+    if (req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password){
+            res.json('success')
+        }else{
+            res.status(400).json('error logging in')
+        }
+    
+        
 })
 
 
